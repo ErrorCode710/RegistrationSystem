621f70bc079c797eb4a0cbb18d7035de2857de39
Error710 <jhunrey.canasa@ctu.edu.ph>
2025-04-29 19:39:10 +0800

    DONE DELETE BUTTON

--- RegistrationSystem/ViewModels/UserPageViewModel.cs
+++ RegistrationSystem/ViewModels/UserPageViewModel.cs
@@ -7,19 +7,20 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Avalonia.Threading;
-using ReactiveUI;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using RegistrationSystem.Models;
 
 
 
 namespace RegistrationSystem.ViewModels
 {
-   public  class UserPageViewModel : ViewModelBase
+   public partial class UserPageViewModel : ViewModelBase
     {
         public ObservableCollection<Userdb> Users {get;}
 
         //public ICommand EditCommand { get;}
-        public ICommand DeleteCommand { get; }
+        //public ICommand DeleteCommand { get; }
 
         public UserPageViewModel() {
             var userManager = new UserManager();
@@ -33,25 +34,37 @@ namespace RegistrationSystem.ViewModels
             userManager.ListAllUser();
 
 
-            DeleteCommand = ReactiveCommand.Create<Userdb>(user =>
+            //DeleteCommand = ReactiveCommand.Create<Userdb>(user =>
+            //{
+
+            //    //Users.Remove(user);
+
+            //    Dispatcher.UIThread.Post(() =>
+            //    {
+            //        Debug.WriteLine($"Deleting user: {user?.UserName ?? "null"}");
+
+            //    });
+            //});
+
+
+            
+
+
+
+            }
+
+        [RelayCommand]
+        private void Delete(Userdb user)
+        {
+            if (user == null) return;
+
+            Dispatcher.UIThread.Post(() =>
             {
-             
-                //Users.Remove(user);
-
-                Dispatcher.UIThread.Post(() =>
-                {
-                    Debug.WriteLine($"Deleting user: {user?.UserName ?? "null"}");
-                    if (user != null) Users.Remove(user);
-                });
+                Debug.WriteLine($"Deleting user: {user.UserName}");
+                Users.Remove(user);
             });
-               
-
-
-
-
-
         }
-       
+
 
     }
 }
